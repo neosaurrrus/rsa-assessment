e6e59ec0b7704c2f62228d8f00a871fea78eba6c
Lukie Kang <lukiek@gmail.com>
2023-12-06 12:44:44 +0000

    build: add font and colour support

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['"Open Sans"', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'rsaBrand': '#5C1380',
+        'rsaAccent': '#D4007F',
+      },
+    },
   },
   plugins: [],
 }
